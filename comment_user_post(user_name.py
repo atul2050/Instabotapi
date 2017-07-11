ad3f19cda3971d64ca1dd4d6e4672be5aa888e7b
atul2050 <atulsailor2050@gmail.com>
2017-07-11 17:44:11 -0500

    added two files one to check users comment and second to search for users comments

---
+++ comment_user_post(user_name.py
@@ -0,0 +1,17 @@
+#This imorts the request
+import requests
+#We import base url and access token from another file called constant
+from constants import base_url ,access_token
+
+
+def comment_user_post(user_name):
+    post_id = latest_post(user_name)
+    request_url = (base_url + "/media/%s/comments?access_token=%s") % (post_id, access_token)
+    request = requests.post(request_url).json()
+    if request['meta']['code'] == 200:
+        print("comment added ")
+        print request  ["data"]["text"]
+    else:
+        print("comment not added try again")
+     #return request["data"]["id"]
+
---
+++ search_comments(user_name).py
@@ -0,0 +1,14 @@
+#This imorts the request
+import requests
+#We import base url and access token from another file called constant
+from constants import base_url ,access_token
+
+
+
+#for searching the comment
+def search_comments(user_name):
+    post_id = latest_post(user_name)
+    request_url = base_url + "/media/" + post_id + "/comments?access_token=" + access_token
+    request_comment = requests.get(request_url).json()
+    return request_comment["data"][0]["id"]
+        # print request_comment["data"][0]["text"]
