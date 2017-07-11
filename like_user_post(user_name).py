a2ed015cc1821ec908678feac6fff5f5467433d1
atul2050 <atulsailor2050@gmail.com>
2017-07-11 17:30:24 -0500

    This file is added to check the likes on user post

---
+++ like_user_post(user_name).py
@@ -0,0 +1,13 @@
+
+
+#it is for making the like on te user id
+def like_user_post(user_name):
+    post_id = latest_post(user_name)
+    payload = {"access_token": access_token}
+    request_url = base_url + "/media/" + post_id + "/likes"
+    response_to_like = requests.post(request_url, payload).json()
+    if response_to_like['meta']['code'] == 200:
+        print ("pic has been liked")
+        # print response_to_like
+    else:
+        print("Something went wrong! Please do it again")
